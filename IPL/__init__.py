ad8b61e87bf3caa87007c5dd4232b994460c52ec
rakshith2000 <gowdarakshith407@gmail.com>
2026-03-29 10:57:33 +0530

    Updated init file

--- IPL/__init__.py
+++ IPL/__init__.py
@@ -70,7 +70,7 @@ def create_app():
             except Exception as e:
                 print(f"Error updating qualifications: {e}")
 
-    @scheduler.task('interval', id='toppers_task', minutes=15, misfire_grace_time=120)
+    @scheduler.task('interval', id='toppers_task', minutes=5, misfire_grace_time=120)
     def update_toppers_task():
         with app.app_context():
             try:
