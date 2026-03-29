81977b5fb0204a5c164ada366a621b86afb86c7a
rakshith2000 <gowdarakshith407@gmail.com>
2026-03-29 12:22:57 +0530

    Updated

--- IPL/main.py
+++ IPL/main.py
@@ -145,7 +145,10 @@ def update_toppers():
     for role, url in tp_urls.items():
         try:
             print(url)
-            response = requests.get(url, timeout=10, verify=False)
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            }
+            response = requests.get(url, headers=headers, timeout=10, verify=False)
             response.raise_for_status()
             html_content = response.text
             parser = lxml.etree.HTMLParser()
